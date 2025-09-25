b9ab5536e287e6023e31892427837d2a0065f0aa
DESKTOP-R94SMKK\2111d <khanhtrantuc2004@gmail.com>
2025-09-25 11:04:39 +0700

    [FEAT] Add Admin Layout

---
+++ CarManagementSystem.DataAccess/Repositories/Interfaces/IVehicleCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Linq.Expressions;
+
+
+namespace CarManagementSystem.DataAccess.Repositories.Interfaces
+{
+    public interface IVehicleCategoryRepository
+    {
+        Task<VehicleCategory> GetByIdAsync(int id);
+        Task<List<VehicleCategory>> GetAllAsync(Expression<Func<VehicleCategory, bool>>? predicate = null);
+
+        Task<VehicleCategory> CreateAsync(VehicleCategory category);
+        Task<VehicleCategory> UpdateAsync(VehicleCategory category);
+        Task<bool> DeleteAsync(int id);
+
+        Task<int> SaveChangesAsync();
+    }
+}
