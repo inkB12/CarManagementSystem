a38b5a9a4c8ee4a07375276ed077b8d1c8efcfbd
dtvgaming214 <dtvgaming214@gmail.com>
2025-09-26 14:33:15 +0700

    Fix some bug of Homepage

---
+++ CarManagementSystem/Controllers/ElectricVehiclesController.cs
@@ -0,0 +1,42 @@
+﻿using CarManagementSystem.DataAccess.Repositories.Interfaces;
+using CarManagementSystem.Services.Interfaces;
+using CarManagementSystem.WebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarManagementSystem.WebMVC.Controllers
+{
+    public class ElectricVehiclesController : Controller
+    {
+        private readonly IElectricVehicleService _service;
+
+        public ElectricVehiclesController(IElectricVehicleService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index(int page = 1, int pageSize = 9)
+        {
+            var all = await _service.GetAllAsync();
+            var totalCount = all.Count;
+
+            var items = all.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            var vm = new ElectricVehicleListViewModel
+            {
+                Vehicles = items,
+                CurrentPage = page,
+                TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize)
+            };
+
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var vehicle = await _service.GetByIdAsync(id);
+            if (vehicle == null) return NotFound();
+
+            return View(vehicle);
+        }
+    }
+}
---
+++ CarManagementSystem/Models/ElectricVehicleListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CarManagementSystem.DataAccess;
+
+namespace CarManagementSystem.WebMVC.Models
+{
+    public class ElectricVehicleListViewModel
+    {
+        public List<ElectricVehicle> Vehicles { get; set; } = new();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
